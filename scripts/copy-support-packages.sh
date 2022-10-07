839de1984091e63ffba7ca77e75c15ba8bd57599
Tim Stahlhut <stahta01@gmail.com>
2022-10-07 19:09:18 -0400

    scripts: Edit copy-support-packages.sh

--- scripts/copy-support-packages.sh
+++ scripts/copy-support-packages.sh
@@ -1,8 +1,7 @@
 mkdir -p support_packages
 
 rm -f ./support_packages/*.zst
-cp -f ../opt-mingw-expat/opt-mingw-x86_64-expat-2.4.9-1-any.pkg.tar.zst ./support_packages/
-cp -f ../opt-mingw-gmp/opt-mingw-x86_64-gmp-6.2.1-1-any.pkg.tar.zst ./support_packages/
+#cp -f ../opt-mingw-gmp/opt-mingw-x86_64-gmp-6.2.1-1-any.pkg.tar.zst ./support_packages/
 cp -f ../opt-mingw-libiconv/opt-mingw-x86_64-libiconv-1.17-1-any.pkg.tar.zst ./support_packages/
 cp -f ../opt-mingw-zlib/opt-mingw-x86_64-zlib-1.2.12-1-any.pkg.tar.zst ./support_packages/
 cp -f ../opt-mingw-pkgconf/opt-mingw-x86_64-pkgconf-1.9.3-1-any.pkg.tar.zst ./support_packages/
