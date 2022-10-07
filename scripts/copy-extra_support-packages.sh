63d4c19a453c37170f6c070467a5bceae0eb0939
Tim Stahlhut <stahta01@gmail.com>
2022-10-07 19:09:17 -0400

    scripts: Add copy-extra_support-packages.sh

---
+++ scripts/copy-extra_support-packages.sh
@@ -0,0 +1,6 @@
+mkdir -p extra_support_packages
+
+rm -f ./extra_support_packages/*.zst
+cp -f ../opt-mingw-expat/opt-mingw-x86_64-expat-2.4.9-1-any.pkg.tar.zst ./extra_support_packages/
+#cp -f ../opt-mingw-mpfr/opt-mingw-x86_64-mpfr-4.1.0.p13-1-any.pkg.tar.zst ./extra_support_packages/
+#cp -f ../opt-mingw-mpc/opt-mingw-x86_64-mpc-1.2.1-1-any.pkg.tar.zst ./extra_support_packages/
