fee8cd6a513da347f78cf04f69f4bbcea10258ef
Tim Stahlhut <stahta01@gmail.com>
2022-10-24 09:37:50 -0400

    scripts: Add copy-cross-toolchain-packages.sh

---
+++ scripts/copy-cross-toolchain-packages.sh
@@ -0,0 +1,7 @@
+mkdir -p cross_gcc4.9_toolchain_mingw_packages
+
+rm -f ./cross_gcc4.9_toolchain_mingw_packages/*.zst
+cp -f ../mingw-cross-i686-w64-mingw32-binutils/opt-mingw-x86_64-i686-w64-mingw32-binutils-2.24-1-any.pkg.tar.zst ./cross_gcc4.9_toolchain_mingw_packages/
+cp -f ../mingw-cross-i686-w64-mingw32-headers/opt-mingw-x86_64-i686-w64-mingw32-headers-git-3.4.0.3935.8c7e00886f69-1-any.pkg.tar.zst ./cross_gcc4.9_toolchain_mingw_packages/
+cp -f ../mingw-cross-i686-w64-mingw32-gcc/opt-mingw-x86_64-i686-w64-mingw32-gcc-4.9.4-1-any.pkg.tar.zst ./cross_gcc4.9_toolchain_mingw_packages/
+cp -f ../mingw-cross-i686-w64-mingw32-crt/opt-mingw-x86_64-i686-w64-mingw32-crt-git-3.4.0.3935.8c7e00886f69-1-any.pkg.tar.zst ./cross_gcc4.9_toolchain_mingw_packages/
