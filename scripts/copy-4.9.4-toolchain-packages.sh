17e24ff0504d222ccf200838f48e9b6e3b267737
Tim Stahlhut <stahta01@gmail.com>
2022-10-25 12:12:00 -0400

    scripts: Edit copy-4.9.4-toolchain-packages.sh

--- scripts/copy-4.9.4-toolchain-packages.sh
+++ scripts/copy-4.9.4-toolchain-packages.sh
@@ -15,8 +15,9 @@ mkdir -p gcc4.9.4_toolchain_mingw_32_packages
 rm -f ./gcc4.9.4_toolchain_mingw_32_packages/*.zst
 cp -f ../opt-mingw-crt3/opt-mingw-i686-headers-git-3.4.0.3935.8c7e00886f69-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-crt3/opt-mingw-i686-crt-git-3.4.0.3935.8c7e00886f69-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-winpthreads3/opt-mingw-i686-libwinpthread-git-3.4.0.3935.8c7e00886f69-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-winpthreads3/opt-mingw-i686-winpthreads-git-3.4.0.3935.8c7e00886f69-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-crt3/opt-mingw-i686-pthread-stub-headers-git-3.4.0.3935.8c7e00886f69-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
+#cp -f ../opt-mingw-winpthreads3/opt-mingw-i686-libwinpthread-git-3.4.0.3935.8c7e00886f69-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
+#cp -f ../opt-mingw-winpthreads3/opt-mingw-i686-winpthreads-git-3.4.0.3935.8c7e00886f69-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-binutils-git/opt-mingw-i686-binutils-git-2.24+r78636.c2.gcd3813a8c9e5-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-gcc-git/opt-mingw-i686-gcc-libs-git-4.9.4+d20160726.r51.c10.g4f18db57daff-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-gcc-git/opt-mingw-i686-gcc-git-4.9.4+d20160726.r51.c10.g4f18db57daff-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
