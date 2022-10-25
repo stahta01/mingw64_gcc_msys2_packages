93df6e7e526aa3dd7f67dad7276085ce8f99735c
Tim Stahlhut <stahta01@gmail.com>
2022-10-25 12:12:03 -0400

    scripts: Edit copy-extra_support-packages.sh

--- scripts/copy-extra_support-packages.sh
+++ scripts/copy-extra_support-packages.sh
@@ -1,6 +1,11 @@
-mkdir -p extra_support_packages
+mkdir -p extra_support_mingw_64_packages
+rm -f ./extra_support_mingw_64_packages/*.zst
 
-rm -f ./extra_support_packages/*.zst
-cp -f ../opt-mingw-expat/opt-mingw-x86_64-expat-2.4.9-1-any.pkg.tar.zst ./extra_support_packages/
-#cp -f ../opt-mingw-mpfr/opt-mingw-x86_64-mpfr-4.1.0.p13-1-any.pkg.tar.zst ./extra_support_packages/
-#cp -f ../opt-mingw-mpc/opt-mingw-x86_64-mpc-1.2.1-1-any.pkg.tar.zst ./extra_support_packages/
+cp -f ../opt-mingw-expat/opt-mingw-x86_64-expat-2.4.9-1-any.pkg.tar.zst ./extra_support_mingw_64_packages/
+#cp -f ../opt-mingw-mpfr/opt-mingw-x86_64-mpfr-4.1.0.p13-1-any.pkg.tar.zst ./extra_support_mingw_64_packages/
+#cp -f ../opt-mingw-mpc/opt-mingw-x86_64-mpc-1.2.1-1-any.pkg.tar.zst ./extra_support_mingw_64_packages/
+
+mkdir -p extra_support_mingw_32_packages
+rm -f ./extra_support_mingw_32_packages/*.zst
+
+cp -f ../opt-mingw-zlib/opt-mingw-i686-zlib-1.2.12-1-any.pkg.tar.zst ./extra_support_mingw_32_packages/
