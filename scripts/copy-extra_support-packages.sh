ee9dcc79ba402df9b8491e137555ba05e7651c81
Tim Stahlhut <stahta01@gmail.com>
2022-11-07 21:44:35 -0500

    scripts: Edit copy-extra_support-packages.sh

--- scripts/copy-extra_support-packages.sh
+++ scripts/copy-extra_support-packages.sh
@@ -9,3 +9,4 @@ mkdir -p extra_support_mingw_32_packages
 rm -f ./extra_support_mingw_32_packages/*.zst
 
 cp -f ../opt-mingw-gmp5/opt-mingw-i686-gmp-5.1.3-1-any.pkg.tar.zst ./extra_support_mingw_32_packages/
+cp -f ../opt-mingw-mpfr3/opt-mingw-i686-mpfr-3.1.6-1-any.pkg.tar.zst ./extra_support_mingw_32_packages/
