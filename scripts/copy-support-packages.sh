ff72404fda7066dbb41ca88c6dd5bd80dea03914
Tim Stahlhut <stahta01@gmail.com>
2022-11-08 00:31:22 -0500

    scripts: Edit copy-support-packages.sh

--- scripts/copy-support-packages.sh
+++ scripts/copy-support-packages.sh
@@ -14,6 +14,7 @@ rm -f ./support_mingw_32_packages/*.zst
 cp -f ../opt-mingw-libiconv/opt-mingw-i686-libiconv-1.17-1-any.pkg.tar.zst ./support_mingw_32_packages/
 cp -f ../opt-mingw-zlib/opt-mingw-i686-zlib-1.2.12-1-any.pkg.tar.zst ./support_mingw_32_packages/
 cp -f ../opt-mingw-pkgconf/opt-mingw-i686-pkgconf-1.9.3-1-any.pkg.tar.zst ./support_mingw_32_packages/
-cp -f ../opt-mingw-gettext/opt-mingw-i686-gettext-runtime-0.21-1-any.pkg.tar.zst ./support_mingw_32_packages/
+#cp -f ../opt-mingw-gettext/opt-mingw-i686-gettext-runtime-0.21-1-any.pkg.tar.zst ./support_mingw_32_packages/
 cp -f ../opt-mingw-autotools/opt-mingw-i686-autotools-2022.10.05-1-any.pkg.tar.zst ./support_mingw_32_packages/
 cp -f ../opt-mingw-windows-default-manifest/opt-mingw-i686-windows-default-manifest-6.4-1-any.pkg.tar.zst ./support_mingw_32_packages/
+cp -f ../opt-mingw-gettext0.19/opt-mingw-i686-gettext-runtime-0.19.4-1-any.pkg.tar.zst ./support_mingw_32_packages/
