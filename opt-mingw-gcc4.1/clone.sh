d6130e158638f4cc066f6ff966ab24df478e0a5e
Tim Stahlhut <stahta01@gmail.com>
2022-11-11 20:09:45 -0500

    opt-mingw-gcc4.1: Add package

---
+++ opt-mingw-gcc4.1/clone.sh
@@ -0,0 +1,3 @@
+git clone --bare --mirror --dissociate --reference-if-able ../gcc-git \
+  --no-tags --single-branch --branch releases/gcc-4.1 --progress \
+  -- https://github.com/gcc-mirror/gcc.git gcc-git
