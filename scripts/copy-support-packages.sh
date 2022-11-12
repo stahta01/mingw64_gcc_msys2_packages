e61d343d58281ea84e12bfa25f7b194477d2b297
Tim Stahlhut <stahta01@gmail.com>
2022-11-11 21:25:44 -0500

    scripts: Edit copy-support-packages.sh

--- scripts/copy-support-packages.sh
+++ scripts/copy-support-packages.sh
@@ -13,4 +13,4 @@ cp -f ../opt-mingw-libiconv/opt-mingw-i686-libiconv-1.17-1-any.pkg.tar.zst ./sup
 cp -f ../opt-mingw-zlib/opt-mingw-i686-zlib-1.2.12-1-any.pkg.tar.zst ./support_mingw_32_packages/
 #cp -f ../opt-mingw-gettext/opt-mingw-i686-gettext-runtime-0.21-1-any.pkg.tar.zst ./support_mingw_32_packages/
 cp -f ../opt-mingw-windows-default-manifest/opt-mingw-i686-windows-default-manifest-6.4-1-any.pkg.tar.zst ./support_mingw_32_packages/
-cp -f ../opt-mingw-gettext0.19/opt-mingw-i686-gettext-runtime-0.19.4-1-any.pkg.tar.zst ./support_mingw_32_packages/
+cp -f ../opt-mingw-gettext0.19/opt-mingw-i686-gettext-runtime-0.19.8.1-1-any.pkg.tar.zst ./support_mingw_32_packages/
