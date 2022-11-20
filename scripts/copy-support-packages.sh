9b0c9fc1a66ca25cb42e62e07b23d0ec7afcc746
Tim Stahlhut <stahta01@gmail.com>
2022-11-20 04:43:14 -0500

    scripts: Edit copy-support-packages.sh

--- scripts/copy-support-packages.sh
+++ scripts/copy-support-packages.sh
@@ -9,8 +9,7 @@ cp -f ../opt-mingw-windows-default-manifest/opt-mingw-x86_64-windows-default-man
 mkdir -p support_mingw_32_packages
 
 rm -f ./support_mingw_32_packages/*.zst
-cp -f ../opt-mingw-libiconv/opt-mingw-i686-libiconv-1.17-1-any.pkg.tar.zst ./support_mingw_32_packages/
-cp -f ../opt-mingw-zlib/opt-mingw-i686-zlib-1.2.12-1-any.pkg.tar.zst ./support_mingw_32_packages/
-#cp -f ../opt-mingw-gettext/opt-mingw-i686-gettext-runtime-0.21-1-any.pkg.tar.zst ./support_mingw_32_packages/
-cp -f ../opt-mingw-windows-default-manifest/opt-mingw-i686-windows-default-manifest-6.4-1-any.pkg.tar.zst ./support_mingw_32_packages/
-cp -f ../opt-mingw-gettext0.19/opt-mingw-i686-gettext-runtime-0.19.8.1-1-any.pkg.tar.zst ./support_mingw_32_packages/
+cp -f ../opt-mingw-libiconv/opt-mingw-i686-libiconv-1.17-2-any.pkg.tar.zst ./support_mingw_32_packages/
+cp -f ../opt-mingw-zlib/opt-mingw-i686-zlib-1.2.12-2-any.pkg.tar.zst ./support_mingw_32_packages/
+cp -f ../opt-mingw-windows-default-manifest/opt-mingw-i686-windows-default-manifest-6.4-2-any.pkg.tar.zst ./support_mingw_32_packages/
+cp -f ../opt-mingw-gettext0.19/opt-mingw-i686-gettext-runtime-0.19.8.1-2-any.pkg.tar.zst ./support_mingw_32_packages/
