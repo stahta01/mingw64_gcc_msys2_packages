88bc369106ed8aa71a0985ee6309cd5f76db6a3b
Tim Stahlhut <stahta01@gmail.com>
2022-11-20 10:08:18 -0500

    scripts: Edit copy-4.3.6-toolchain-packages.sh

--- scripts/copy-4.3.6-toolchain-packages.sh
+++ scripts/copy-4.3.6-toolchain-packages.sh
@@ -1,7 +1,7 @@
 mkdir -p gcc4.3.6_toolchain_mingw_32_packages
 
 rm -f ./gcc4.3.6_toolchain_mingw_32_packages/*.zst
-cp -f ../opt-mingw-crt2/opt-mingw-i686-headers-git-2.0.0.2656.06c5a4241b47-1-any.pkg.tar.zst ./gcc4.3.6_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-crt2/opt-mingw-i686-crt-git-2.0.0.2656.06c5a4241b47-1-any.pkg.tar.zst ./gcc4.3.6_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-binutils2.18/opt-mingw-i686-binutils-git-2.18+r55352.c0.gbee8c06e0fcb-1-any.pkg.tar.zst ./gcc4.3.6_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-gcc4.3/opt-mingw-i686-gcc-git-4.3.6+d20110627.r39.c8.g6503c21eb909-1-any.pkg.tar.zst ./gcc4.3.6_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-crt2/opt-mingw-i686-headers-git-2.0.0.2656.06c5a4241b47-2-any.pkg.tar.zst ./gcc4.3.6_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-crt2/opt-mingw-i686-crt-git-2.0.0.2656.06c5a4241b47-2-any.pkg.tar.zst ./gcc4.3.6_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-binutils2.18/opt-mingw-i686-binutils-git-2.18+r55352.c0.gbee8c06e0fcb-2-any.pkg.tar.zst ./gcc4.3.6_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-gcc4.3/opt-mingw-i686-gcc-git-4.3.6+d20110627.r39.c8.g6503c21eb909-2-any.pkg.tar.zst ./gcc4.3.6_toolchain_mingw_32_packages/
