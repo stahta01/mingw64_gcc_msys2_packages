936183bb90d73116ae58279c0d414ead91d4b4d6
Tim Stahlhut <stahta01@gmail.com>
2022-11-27 18:33:22 -0500

    opt-mingw-binutils2.25: Add package

---
+++ opt-mingw-binutils2.25/clone.sh
@@ -0,0 +1,3 @@
+git clone --bare --mirror --dissociate --reference-if-able ../binutils-gdb-git \
+  --no-tags --single-branch --branch binutils-2_25-branch --progress \
+  -- https://sourceware.org/git/binutils-gdb binutils-git
