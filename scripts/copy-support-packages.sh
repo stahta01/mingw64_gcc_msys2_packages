57134088cc93b27c9b2471e70573a613d7004127
Tim Stahlhut <stahta01@gmail.com>
2022-11-30 11:56:53 -0500

    scripts: Edit copy-support-packages.sh

--- scripts/copy-support-packages.sh
+++ scripts/copy-support-packages.sh
@@ -1,10 +1,10 @@
 mkdir -p support_mingw_64_packages
 
 rm -f ./support_mingw_64_packages/*.zst
-cp -f ../opt-mingw-libiconv/opt-mingw-x86_64-libiconv-1.17-1-any.pkg.tar.zst ./support_mingw_64_packages/
-cp -f ../opt-mingw-zlib/opt-mingw-x86_64-zlib-1.2.12-1-any.pkg.tar.zst ./support_mingw_64_packages/
-cp -f ../opt-mingw-gettext/opt-mingw-x86_64-gettext-runtime-0.21-1-any.pkg.tar.zst ./support_mingw_64_packages/
-cp -f ../opt-mingw-windows-default-manifest/opt-mingw-x86_64-windows-default-manifest-6.4-1-any.pkg.tar.zst ./support_mingw_64_packages/
+cp -f ../opt-mingw-libiconv/opt-mingw-x86_64-libiconv-1.17-2-any.pkg.tar.zst ./support_mingw_64_packages/
+cp -f ../opt-mingw-zlib/opt-mingw-x86_64-zlib-1.2.13-1-any.pkg.tar.zst ./support_mingw_64_packages/
+cp -f ../opt-mingw-windows-default-manifest/opt-mingw-x86_64-windows-default-manifest-6.4-2-any.pkg.tar.zst ./support_mingw_64_packages/
+cp -f ../opt-mingw-gettext0.19/opt-mingw-x86_64-gettext-runtime-0.19.8.1-2-any.pkg.tar.zst ./support_mingw_64_packages/
 
 mkdir -p support_mingw_32_packages
 
