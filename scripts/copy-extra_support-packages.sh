2f0f7984b0367a32fa2d9a1b5ebaa7a913595c2e
Tim Stahlhut <stahta01@gmail.com>
2022-11-30 11:20:40 -0500

    scripts: Edit copy-extra_support-packages.sh

--- scripts/copy-extra_support-packages.sh
+++ scripts/copy-extra_support-packages.sh
@@ -4,7 +4,7 @@ rm -f ./extra_support_mingw_64_packages/*.zst
 cp -f ../opt-mingw-expat/opt-mingw-x86_64-expat-2.4.9-1-any.pkg.tar.zst ./extra_support_mingw_64_packages/
 #cp -f ../opt-mingw-mpfr/opt-mingw-x86_64-mpfr-4.1.0.p13-1-any.pkg.tar.zst ./extra_support_mingw_64_packages/
 #cp -f ../opt-mingw-mpc/opt-mingw-x86_64-mpc-1.2.1-1-any.pkg.tar.zst ./extra_support_mingw_64_packages/
-cp -f ../opt-mingw-pkgconf/opt-mingw-x86_64-pkgconf-1.9.3-1-any.pkg.tar.zst ./extra_support_mingw_64_packages/
+cp -f ../opt-mingw-pkgconf/opt-mingw-x86_64-pkgconf-1.9.3-2-any.pkg.tar.zst ./extra_support_mingw_64_packages/
 cp -f ../opt-mingw-autotools/opt-mingw-x86_64-autotools-2022.10.05-1-any.pkg.tar.zst ./extra_support_mingw_64_packages/
 
 mkdir -p extra_support_mingw_32_packages
