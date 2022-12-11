eb2d49b63fa7b8b143cc8285cf13703b1e524c94
Tim Stahlhut <stahta01@gmail.com>
2022-12-11 00:40:20 -0500

    Edit clone-binutils.sh add missing CD

--- clone-binutils.sh
+++ clone-binutils.sh
@@ -1,5 +1,6 @@
 git clone --no-tags --single-branch --branch master --progress \
   -- https://sourceware.org/git/binutils-gdb binutils-gdb-git && \
+cd binutils-gdb-git && \
 git remote remove origin && \
 git remote add origin https://sourceware.org/git/binutils-gdb && \
 git fetch origin binutils-2_18-branch && git checkout binutils-2_18-branch && \
