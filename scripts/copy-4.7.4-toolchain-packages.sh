853794d3f2dd5455174c51eaf06358224ff153a9
Tim Stahlhut <stahta01@gmail.com>
2022-12-11 23:52:11 -0500

    scripts: Re-wrote copy-4.7.4-toolchain-packages.sh

--- scripts/copy-4.7.4-toolchain-packages.sh
+++ scripts/copy-4.7.4-toolchain-packages.sh
@@ -1,9 +1,9 @@
 mkdir -p gcc4.7.4_toolchain_mingw_32_packages
 
 rm -f ./gcc4.7.4_toolchain_mingw_32_packages/*.zst
-cp -f ../opt-mingw-crt2.0.9/opt-mingw-i686-headers-git-2.0.9.2715.ea32a0c5bf6f-1-any.pkg.tar.zst ./gcc4.6.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-crt2.0.9/opt-mingw-i686-crt-git-2.0.9.2715.ea32a0c5bf6f-1-any.pkg.tar.zst ./gcc4.6.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-binutils2.25/opt-mingw-i686-binutils-git-2.25+r82139.c5.g8f2db47d13b6-1-any.pkg.tar.zst ./gcc4.6.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-gcc4.7/opt-mingw-i686-gcc-libs-git-4.7.4+d20140612.r45.c9.g4d1f511d7f2f-4-any.pkg.tar.zst ./gcc4.7.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-gcc4.7/opt-mingw-i686-gcc-git-4.7.4+d20140612.r45.c9.g4d1f511d7f2f-4-any.pkg.tar.zst ./gcc4.7.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-gcc4.7/opt-mingw-i686-gcc-ada-git-4.7.4+d20140612.r45.c9.g4d1f511d7f2f-4-any.pkg.tar.zst ./gcc4.7.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-crt2.0.9/opt-mingw-i686-headers-git-2.0.9.2715.ea32a0c5bf6f-1-any.pkg.tar.zst ./gcc4.7.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-crt2.0.9/opt-mingw-i686-crt-git-2.0.9.2715.ea32a0c5bf6f-1-any.pkg.tar.zst ./gcc4.7.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-binutils2.25/opt-mingw-i686-binutils-git-2.25+r82134.c5.g8f2db47d13b6-1-any.pkg.tar.zst ./gcc4.7.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-gcc4.7/opt-mingw-i686-gcc-libs-git-4.7.4+d20140612.r45.c9.g4d1f511d7f2f-5-any.pkg.tar.zst ./gcc4.7.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-gcc4.7/opt-mingw-i686-gcc-git-4.7.4+d20140612.r45.c9.g4d1f511d7f2f-5-any.pkg.tar.zst ./gcc4.7.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-gcc4.7/opt-mingw-i686-gcc-ada-git-4.7.4+d20140612.r45.c9.g4d1f511d7f2f-5-any.pkg.tar.zst ./gcc4.7.4_toolchain_mingw_32_packages/
