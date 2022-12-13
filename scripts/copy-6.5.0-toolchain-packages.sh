52605a57dce9bf0fa24fec0429c4b16696705c70
Tim Stahlhut <stahta01@gmail.com>
2022-12-13 00:14:19 -0500

    scripts: Edit copy-6.5.0-toolchain-packages.sh

--- scripts/copy-6.5.0-toolchain-packages.sh
+++ scripts/copy-6.5.0-toolchain-packages.sh
@@ -14,9 +14,9 @@ cp -f ../opt-mingw-gcc6/opt-mingw-x86_64-gcc-ada-git-6.5.0+d20181026.r2.c13.g39a
 mkdir -p gcc6.5.0_toolchain_mingw_32_packages
 
 rm -f ./gcc6.5.0_toolchain_mingw_32_packages/*.zst
-cp -f ../opt-mingw-crt6/opt-mingw-i686-headers-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc6.5.0_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-crt6/opt-mingw-i686-crt-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc6.5.0_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-crt6/opt-mingw-i686-pthread-stub-headers-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc6.5.0_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-crt5/opt-mingw-i686-headers-git-5.0.0.4746.715ecca90d57-1-any.pkg.tar.zst ./gcc6.5.0_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-crt5/opt-mingw-i686-crt-git-5.0.0.4746.715ecca90d57-1-any.pkg.tar.zst ./gcc6.5.0_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-crt5/opt-mingw-i686-pthread-stub-headers-git-5.0.0.4746.715ecca90d57-1-any.pkg.tar.zst ./gcc6.5.0_toolchain_mingw_32_packages/
 #cp -f ../opt-mingw-winpthreads6/opt-mingw-i686-libwinpthread-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc6.5.0_toolchain_mingw_32_packages/
 #cp -f ../opt-mingw-winpthreads6/opt-mingw-i686-winpthreads-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc6.5.0_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-binutils2.27/opt-mingw-i686-binutils-git-2.27+r88001.c1.gb5d3ac25628b-1-any.pkg.tar.zst ./gcc6.5.0_toolchain_mingw_32_packages/
