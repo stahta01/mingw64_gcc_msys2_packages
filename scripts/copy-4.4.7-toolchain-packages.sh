6c98eebc4f592233e07c8eecd5b1adeab4ff917c
Tim Stahlhut <stahta01@gmail.com>
2022-12-27 22:13:25 -0500

    scripts: Edit copy-4.4.7-toolchain-packages.sh

--- scripts/copy-4.4.7-toolchain-packages.sh
+++ scripts/copy-4.4.7-toolchain-packages.sh
@@ -4,5 +4,6 @@ rm -f ./gcc4.4.7_toolchain_mingw_32_packages/*.zst
 cp -f ../opt-mingw-crt2.0.6/opt-mingw-i686-headers-git-2.0.6.2656.06c5a4241b47-1-any.pkg.tar.zst ./gcc4.4.7_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-crt2.0.6/opt-mingw-i686-crt-git-2.0.6.2656.06c5a4241b47-1-any.pkg.tar.zst ./gcc4.4.7_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-binutils2.18/opt-mingw-i686-binutils-git-2.18+r55352.c0.gbee8c06e0fcb-2-any.pkg.tar.zst ./gcc4.4.7_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-gcc4.4/opt-mingw-i686-gcc-git-4.4.7+d20120313.r40.c6.g086e1860db96-2-any.pkg.tar.zst ./gcc4.4.7_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-zlib/opt-mingw-i686-zlib-1.2.13-2-any.pkg.tar.zst ./gcc4.4.7_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-gcc4.4/opt-mingw-i686-gcc-git-4.4.7+d20120313.r40.c6.g086e1860db96-3-any.pkg.tar.zst ./gcc4.4.7_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-gcc4.4/opt-mingw-i686-gcc-libs-git-4.4.7+d20120313.r40.c6.g086e1860db96-3-any.pkg.tar.zst ./gcc4.4.7_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-zlib/opt-mingw-i686-zlib-1.2.13-3-any.pkg.tar.zst ./gcc4.4.7_toolchain_mingw_32_packages/
