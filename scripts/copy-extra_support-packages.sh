1611ee3d947517f99d592700ce045d001e1c64e1
Tim Stahlhut <stahta01@gmail.com>
2022-12-31 06:02:45 -0500

    scripts: Edit copy-extra_support-packages.sh

--- scripts/copy-extra_support-packages.sh
+++ scripts/copy-extra_support-packages.sh
@@ -2,11 +2,11 @@ mkdir -p extra_support_mingw_64_packages
 rm -f ./extra_support_mingw_64_packages/*.zst
 
 #cp -f ../opt-mingw-expat/opt-mingw-x86_64-expat-2.4.9-1-any.pkg.tar.zst ./extra_support_mingw_64_packages/
-cp -f ../opt-mingw-pkgconf/opt-mingw-x86_64-pkgconf-1.9.3-2-any.pkg.tar.zst ./extra_support_mingw_64_packages/
+cp -f ../opt-mingw-pkgconf/opt-mingw-x86_64-pkgconf-1.9.3-3-any.pkg.tar.zst ./extra_support_mingw_64_packages/
 cp -f ../opt-mingw-autotools/opt-mingw-x86_64-autotools-2022.10.05-1-any.pkg.tar.zst ./extra_support_mingw_64_packages/
 
 mkdir -p extra_support_mingw_32_packages
 rm -f ./extra_support_mingw_32_packages/*.zst
 
-cp -f ../opt-mingw-pkgconf/opt-mingw-i686-pkgconf-1.9.3-2-any.pkg.tar.zst ./extra_support_mingw_32_packages/
+cp -f ../opt-mingw-pkgconf/opt-mingw-i686-pkgconf-1.9.3-3-any.pkg.tar.zst ./extra_support_mingw_32_packages/
 cp -f ../opt-mingw-autotools/opt-mingw-i686-autotools-2022.10.05-1-any.pkg.tar.zst ./extra_support_mingw_32_packages/
