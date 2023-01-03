bad6ca2823a94265f111916ab14a7a82c51621e2
Tim Stahlhut <stahta01@gmail.com>
2023-01-03 10:23:19 -0500

    scripts: Edit copy-4.5.4-toolchain-packages.sh

--- scripts/copy-4.5.4-toolchain-packages.sh
+++ scripts/copy-4.5.4-toolchain-packages.sh
@@ -1,10 +1,9 @@
 mkdir -p gcc4.5.4_toolchain_mingw_32_packages
 
 rm -f ./gcc4.5.4_toolchain_mingw_32_packages/*.zst
-cp -f ../opt-mingw-crt2.0.7/opt-mingw-i686-headers-git-2.0.7.2668.56b79e1c0757-1-any.pkg.tar.zst ./gcc4.5.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-crt2.0.7/opt-mingw-i686-crt-git-2.0.7.2668.56b79e1c0757-1-any.pkg.tar.zst ./gcc4.5.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-binutils2.23/opt-mingw-i686-binutils-git-2.23+r74545.c0.g9f514a29752a-2-any.pkg.tar.zst ./gcc4.5.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-gcc4.5/opt-mingw-i686-gcc-libs-git-4.5.4+d20120702.r42.c7.g41eccc86828f-2-any.pkg.tar.zst ./gcc4.5.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-gcc4.5/opt-mingw-i686-gcc-git-4.5.4+d20120702.r42.c7.g41eccc86828f-2-any.pkg.tar.zst ./gcc4.5.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-gcc4.5/opt-mingw-i686-gcc-ada-git-4.5.4+d20120702.r42.c7.g41eccc86828f-2-any.pkg.tar.zst ./gcc4.5.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-zlib/opt-mingw-i686-zlib-1.2.13-3-any.pkg.tar.zst ./gcc4.5.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-crt2.0.7/opt-mingw-i686-headers-git-2.0.7.2668.56b79e1c0757-2-any.pkg.tar.zst ./gcc4.5.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-crt2.0.7/opt-mingw-i686-crt-git-2.0.7.2668.56b79e1c0757-2-any.pkg.tar.zst ./gcc4.5.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-binutils2.23/opt-mingw-i686-binutils-git-2.23+r74545.c0.g9f514a29752a-3-any.pkg.tar.zst ./gcc4.5.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-gcc4.5/opt-mingw-i686-gcc-libs-git-4.5.4+d20120702.r42.c7.g41eccc86828f-5-any.pkg.tar.zst ./gcc4.5.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-gcc4.5/opt-mingw-i686-gcc-git-4.5.4+d20120702.r42.c7.g41eccc86828f-5-any.pkg.tar.zst ./gcc4.5.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-gcc4.5/opt-mingw-i686-gcc-ada-git-4.5.4+d20120702.r42.c7.g41eccc86828f-5-any.pkg.tar.zst ./gcc4.5.4_toolchain_mingw_32_packages/
