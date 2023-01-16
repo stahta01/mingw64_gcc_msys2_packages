6d6d8c4fd8da5b85e2498ec17950563d578213cd
Tim Stahlhut <stahta01@gmail.com>
2023-01-16 04:06:52 -0500

    scripts: Edit copy-4.9.4-toolchain-packages.sh

--- scripts/copy-4.9.4-toolchain-packages.sh
+++ scripts/copy-4.9.4-toolchain-packages.sh
@@ -1,8 +1,8 @@
 mkdir -p gcc4.9.4_toolchain_mingw_64_packages
 
 rm -f ./gcc4.9.4_toolchain_mingw_64_packages/*.zst
-cp -f ../opt-mingw-crt6/opt-mingw-x86_64-headers-git-6.0.0.5241.e174bf62d33d-2-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
-cp -f ../opt-mingw-crt6/opt-mingw-x86_64-crt-git-6.0.0.5241.e174bf62d33d-2-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
+cp -f ../opt-mingw-crt6/opt-mingw-x86_64-headers-git-6.0.0.5241.e174bf62d33d-3-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
+cp -f ../opt-mingw-crt6/opt-mingw-x86_64-crt-git-6.0.0.5241.e174bf62d33d-3-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
 #cp -f ../opt-mingw-crt6/opt-mingw-x86_64-pthread-stub-headers-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
 cp -f ../opt-mingw-winpthreads6/opt-mingw-x86_64-libwinpthread-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
 cp -f ../opt-mingw-winpthreads6/opt-mingw-x86_64-winpthreads-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
@@ -14,8 +14,8 @@ cp -f ../opt-mingw-gcc4.9/opt-mingw-x86_64-gcc-ada-git-4.9.4+d20160726.r51.c10.g
 mkdir -p gcc4.9.4_toolchain_mingw_32_packages
 
 rm -f ./gcc4.9.4_toolchain_mingw_32_packages/*.zst
-cp -f ../opt-mingw-crt6/opt-mingw-i686-headers-git-6.0.0.5241.e174bf62d33d-2-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-crt6/opt-mingw-i686-crt-git-6.0.0.5241.e174bf62d33d-2-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-crt6/opt-mingw-i686-headers-git-6.0.0.5241.e174bf62d33d-3-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-crt6/opt-mingw-i686-crt-git-6.0.0.5241.e174bf62d33d-3-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
 #cp -f ../opt-mingw-crt6/opt-mingw-i686-pthread-stub-headers-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-winpthreads6/opt-mingw-i686-libwinpthread-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-winpthreads6/opt-mingw-i686-winpthreads-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
