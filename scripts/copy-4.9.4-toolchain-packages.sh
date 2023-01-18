fe4feb7ea9d3c8c311c63caeb0a5465b3656434e
Tim Stahlhut <stahta01@gmail.com>
2023-01-18 14:31:29 -0500

    scripts: Edit copy-4.9.4-toolchain-packages.sh

--- scripts/copy-4.9.4-toolchain-packages.sh
+++ scripts/copy-4.9.4-toolchain-packages.sh
@@ -6,7 +6,7 @@ cp -f ../opt-mingw-crt6/opt-mingw-x86_64-crt-git-6.0.0.5241.e174bf62d33d-3-any.p
 #cp -f ../opt-mingw-crt6/opt-mingw-x86_64-pthread-stub-headers-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
 cp -f ../opt-mingw-winpthreads6/opt-mingw-x86_64-libwinpthread-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
 cp -f ../opt-mingw-winpthreads6/opt-mingw-x86_64-winpthreads-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
-cp -f ../opt-mingw-binutils2.25/opt-mingw-x86_64-binutils-git-2.25+r82134.c5.g8f2db47d13b6-2-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
+cp -f ../opt-mingw-binutils2.25/opt-mingw-x86_64-binutils-git-2.25+r82134.c5.g8f2db47d13b6-3-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
 cp -f ../opt-mingw-gcc4.9/opt-mingw-x86_64-gcc-libs-git-4.9.4+d20160726.r51.c10.g4f18db57daff-5-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
 cp -f ../opt-mingw-gcc4.9/opt-mingw-x86_64-gcc-git-4.9.4+d20160726.r51.c10.g4f18db57daff-5-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
 cp -f ../opt-mingw-gcc4.9/opt-mingw-x86_64-gcc-ada-git-4.9.4+d20160726.r51.c10.g4f18db57daff-5-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_64_packages/
@@ -19,7 +19,7 @@ cp -f ../opt-mingw-crt6/opt-mingw-i686-crt-git-6.0.0.5241.e174bf62d33d-3-any.pkg
 #cp -f ../opt-mingw-crt6/opt-mingw-i686-pthread-stub-headers-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-winpthreads6/opt-mingw-i686-libwinpthread-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-winpthreads6/opt-mingw-i686-winpthreads-git-6.0.0.5241.e174bf62d33d-1-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
-cp -f ../opt-mingw-binutils2.25/opt-mingw-i686-binutils-git-2.25+r82134.c5.g8f2db47d13b6-2-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
+cp -f ../opt-mingw-binutils2.25/opt-mingw-i686-binutils-git-2.25+r82134.c5.g8f2db47d13b6-3-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-gcc4.9/opt-mingw-i686-gcc-libs-git-4.9.4+d20160726.r51.c10.g4f18db57daff-5-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-gcc4.9/opt-mingw-i686-gcc-git-4.9.4+d20160726.r51.c10.g4f18db57daff-5-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
 cp -f ../opt-mingw-gcc4.9/opt-mingw-i686-gcc-ada-git-4.9.4+d20160726.r51.c10.g4f18db57daff-5-any.pkg.tar.zst ./gcc4.9.4_toolchain_mingw_32_packages/
