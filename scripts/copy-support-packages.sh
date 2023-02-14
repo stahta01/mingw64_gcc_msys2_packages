b73e2181bc2943397032843a5ef4ce46f0d63923
Tim Stahlhut <stahta01@gmail.com>
2023-02-14 15:06:49 -0500

    scripts: Edit copy-support-packages.sh add UCRT64

--- scripts/copy-support-packages.sh
+++ scripts/copy-support-packages.sh
@@ -13,3 +13,11 @@ cp -f ../opt-mingw-libiconv/opt-mingw-i686-libiconv-1.17-4-any.pkg.tar.zst ./sup
 cp -f ../opt-mingw-windows-default-manifest/opt-mingw-i686-windows-default-manifest-6.4-3-any.pkg.tar.zst ./support_mingw_32_packages/
 cp -f ../opt-mingw-gettext0.19/opt-mingw-i686-gettext-runtime-0.19.8.1-4-any.pkg.tar.zst ./support_mingw_32_packages/
 cp -f ../opt-mingw-zlib/opt-mingw-i686-zlib-1.2.13-5-any.pkg.tar.zst ./support_mingw_32_packages/
+
+mkdir -p support_ucrt_64_packages
+
+rm -f ./support_ucrt_64_packages/*.zst
+cp -f ../opt-mingw-libiconv/opt-ucrt-x86_64-libiconv-1.17-4-any.pkg.tar.zst ./support_ucrt_64_packages/
+cp -f ../opt-mingw-gettext0.19/opt-ucrt-x86_64-gettext-runtime-0.19.8.1-4-any.pkg.tar.zst ./support_ucrt_64_packages/
+cp -f ../opt-mingw-windows-default-manifest/opt-ucrt-x86_64-windows-default-manifest-6.4-3-any.pkg.tar.zst ./support_ucrt_64_packages/
+cp -f ../opt-mingw-zlib/opt-ucrt-x86_64-zlib-1.2.13-5-any.pkg.tar.zst ./support_ucrt_64_packages/
