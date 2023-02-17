cabc3be2887ee9bf9a6a08c7f67281c6defc4f16
Tim Stahlhut <stahta01@gmail.com>
2023-02-16 21:36:07 -0500

    scripts: Edit copy-4.6.4-toolchain-packages.sh

--- scripts/copy-4.6.4-toolchain-packages.sh
+++ scripts/copy-4.6.4-toolchain-packages.sh
@@ -1,9 +1,9 @@
 mkdir -p gcc4.6.4_toolchain_mingw_64_packages
 
 rm -f ./gcc4.6.4_toolchain_mingw_64_packages/*.zst
-cp -f ../opt-mingw-crt6/opt-mingw-x86_64-headers-git-6.0.0.5241.e174bf62d33d-3-any.pkg.tar.zst ./gcc4.6.4_toolchain_mingw_64_packages/
-cp -f ../opt-mingw-crt6/opt-mingw-x86_64-crt-git-6.0.0.5241.e174bf62d33d-3-any.pkg.tar.zst ./gcc4.6.4_toolchain_mingw_64_packages/
-cp -f ../opt-mingw-crt6/opt-mingw-x86_64-pthread-stub-headers-git-6.0.0.5241.e174bf62d33d-3-any.pkg.tar.zst ./gcc4.6.4_toolchain_mingw_64_packages/
+cp -f ../opt-mingw-crt6/opt-mingw-x86_64-headers-git-6.0.0.5241.e174bf62d33d-4-any.pkg.tar.zst ./gcc4.6.4_toolchain_mingw_64_packages/
+cp -f ../opt-mingw-crt6/opt-mingw-x86_64-crt-git-6.0.0.5241.e174bf62d33d-4-any.pkg.tar.zst ./gcc4.6.4_toolchain_mingw_64_packages/
+cp -f ../opt-mingw-crt6/opt-mingw-x86_64-pthread-stub-headers-git-6.0.0.5241.e174bf62d33d-4-any.pkg.tar.zst ./gcc4.6.4_toolchain_mingw_64_packages/
 cp -f ../opt-mingw-binutils2.24/opt-mingw-x86_64-binutils-git-2.24+r78634.c2.gcd3813a8c9e5-3-any.pkg.tar.zst ./gcc4.6.4_toolchain_mingw_64_packages/
 cp -f ../opt-mingw-gcc4.6/opt-mingw-x86_64-gcc-libs-git-4.6.4+d20130412.r45.c5.g66cc0b3cf5bc-8-any.pkg.tar.zst ./gcc4.6.4_toolchain_mingw_64_packages/
 cp -f ../opt-mingw-gcc4.6/opt-mingw-x86_64-gcc-git-4.6.4+d20130412.r45.c5.g66cc0b3cf5bc-8-any.pkg.tar.zst ./gcc4.6.4_toolchain_mingw_64_packages/
